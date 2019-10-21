b889b1748f73f8e9c5ec9c383b0c3286ff00241e
Long Ricker <philongvadung@gmail.com>
2019-10-21 21:02:04 +0700

    Upload ont to one

--- PHP/03-20191016/Form_data_table/index.php
+++ PHP/03-20191016/Form_data_table/index.php
@@ -9,61 +9,33 @@
   </head>
   <body>
     <?php
-        require_once("connection.php");
-        
-        $sql_data_table = "select * from $dbbase.$table"; // Get all data of table
-        $data_table = $conn->query($sql_data_table);
-        
-        $sql_columns_table = "select COLUMN_NAME from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME= 'users' and table_schema = 'ap_21_3'"; // Get all columns name of table
-        $columns_table = $conn->query($sql_columns_table);
-
-        if(isset($_GET["submit"])){
-            if(trim($_GET["last_name"] != "")){
-                // Check exist of user
-                $id = $_GET["id"];
-                $sql_check = "select * from $dbbase.$table where id = $id";
-                $result = $conn->query($sql_check);
-                if($result->num_rows > 0){
-                   echo "Create Failed";
-                }
-                else{
-                    // Create user if not exist
-                    $last_name = $_GET["last_name"];
-                    $first_name = $_GET["first_name"];
-                    $address = $_GET["address"];
-                    $city = $_GET["city"];
-                    $sql_create_user = "insert into `$dbbase`.`$table` (`last_name`, `first_name`, `address`, `city`) values('$last_name', '$first_name', '$address', '$city');";
-                    $resultelse = $conn->query($sql_create_user);
-                    if($resultelse) {
-                        echo "<script type='text/javascript'>alert('Created users');</script>";
-                        $url = $_SERVER["PHP_SELF"];
-                        header("Location: $url");
-                    }
-                    else echo "<script type='text/javascript'>alert('Create Failed');</script>";
-                }
-
-            }
-            else{
-                // If don't values in form
-                $url = $_SERVER["PHP_SELF"];
-                header("Location: $url");
-                $message = "Khong de trong";
-                echo "<script type='text/javascript'>alert('$message');</script>";
-            }
-        }
-        
+        require('create_user.php');
+        require('sign-in_user.php');
     ?>
-      
+
     <div class="form-sign-in d-flex justify-content-center align-items-center" id="form-sign-in">
         <a class="btn btn-out text-white" id = "close-form" onclick="closeFormSignIn()">X</a>
-        <div class="container bg-white-opacity w-25 h-50 shadow">
+        <div class="container bg-white-opacity w-25 h-75 shadow">
             <form action="#" method="GET" class="flex-column align-items-center h-100 d-flex justify-content-center">
-                <h2 class="pb-4 text-white">Sign in</h2>
+                <h2 class="pb-4 text-white">Create user</h2>
                 <input class="form-control mb-2 text-white bg-dark-opacity shadow" type="text" placeholder="Last name" name = "last_name">
                 <input class="form-control mb-2 text-white bg-dark-opacity shadow" type="text" placeholder="First name" name = "first_name">
                 <input class="form-control mb-2 text-white bg-dark-opacity shadow" type="text" placeholder="Address" name = "address">
-                <input class="form-control mb-4 text-white bg-dark-opacity shadow" type="text" placeholder="City" name = "city">
-                <input class="btn btn-light w-50" type="submit" value="Submit" name="submit">
+                <input class="form-control mb-2 text-white bg-dark-opacity shadow" type="text" placeholder="City" name = "city">
+                <input class="form-control mb-2 text-white bg-dark-opacity shadow" type="text" placeholder="User" name = "user">
+                <input class="form-control mb-2 text-white bg-dark-opacity shadow" type="password" placeholder="Password" name = "pass">
+                <select name="roles_id" class="form-control mb-4 text-white bg-dark-opacity shadow">
+                    <?php   
+                        $table = "roles";
+                        $sql = "select * from $dbase.$table";
+                        $result = $conn->query($sql);
+                        while($row = $result->fetch_assoc()){?>
+                            <option value = "<?php echo $row["roles_id"] ?>"><?php echo $row["name"] ?></option>
+                            <?php
+                        }
+                    ?>
+                </select>
+                <input class="btn btn-light w-50" type="submit" value="Create" name="create">
             </form>
         </div>
     </div>
@@ -91,6 +63,15 @@
                                 <td><?php echo $row["last_name"] ?></td>
                                 <td><?php echo $row["address"] ?></td>
                                 <td><?php echo $row["city"] ?></td>
+                                <td><?php echo $row["user"] ?></td>
+                                <td><?php echo $row["pass"] ?></td>
+                                <td><?php 
+                                    $sql = "select name from ap_21_3.users inner join ap_21_3.roles on ap_21_3.users.roles_id = ap_21_3.roles.roles_id where id = " . $row["id"];
+                                    $result = $conn->query($sql);
+                                    if($result->num_rows>0){
+                                        echo $result->fetch_assoc()["name"];
+                                    }
+                                ?></td>
                             </tr>
                         <?php 
                         }
@@ -100,7 +81,8 @@
         </table>
         <a href="#" class="btn btn-outline-primary" id = "Sign-in-member" onclick="showFormSignIn()">Create user</a>
     </div>
-    <script type="text/javascript" src="./form.js"></script>
+    <script type="text/javascript" src="./form_sign-in.js"></script>
+    <script type="text/javascript" src="./form_create.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
